c101d9cf600d8b5c93dfd8ecb19a281a25b5021e
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-24 15:10:13 +0100

    add documentation

--- 4-print_square.py
+++ 4-print_square.py
@@ -1,7 +1,23 @@
 #!/usr/bin.python3
+"""
+    This module contains a function that prints a square
+
+    Args:
+        size (int): size of square
+
+    Raises:
+        ValueError: if size is less than 0
+        TypeError: if type or instance of size is not an integer
+    Example:
+        >>> print_square(3)
+        ###
+        ###
+        ###
+"""
 
 
 def print_square(size):
+    """Prints square with '#' character"""
 
     type_err = "size must be an integer"
     value_err = "size must be >= 0"
